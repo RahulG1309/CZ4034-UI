5b10958b6389a5b352424789711f73f37f5a3190
Rahul <77380705+RahulG1309@users.noreply.github.com>
2022-04-03 14:55:33 +0800

    Add files via upload

---
+++ search.js
@@ -0,0 +1,91 @@
+async function myFunc() {
+    let url = 'http://localhost:8983/solr/tweets/query?defType=dismax&indent=true&rows=100&df=Text&fl=*,score&debugQuery=true&q.op=OR&q=';
+    let query = document.getElementById('userSearchQuery');
+    //url = url + query.value
+    //console.log(url);
+    url = url + query.value;
+    var div = document.getElementById('resultsDiv');
+    var response = await fetch(url);
+    var data = await response.json();
+    // console.log(data.response)
+    // console.log(data.response.docs)
+    // console.log(data.responseHeader)
+    var noOfDocs = data.response.numFound
+    var myData = data.response.docs
+    // var preparationTime = data.debug.timing.prepare.query.time;
+    // var processingTime = data.debug.timing.process.query.time;
+    // console.log(data.debug);
+    var querySpeed = data.debug.timing.time;
+    //To measure the speed of querying
+    // let displayColumns = ['name', 'username', 'text', 'retweet_count', 'like_count', 'score'];
+    let displayColumns = ['Name', 'Username', 'Text', 'score'];
+    myData = myData.map(x => {
+      let newObj = {};
+      for (col of displayColumns) {
+        newObj[col] = x[col];
+      }
+      return newObj;
+    });
+    var col = [];
+    for (var i = 0; i < myData.length; i++) {
+      for (var key in myData[i]) {
+        if (col.indexOf(key) === -1) {
+          col.push(key);
+        }
+      }
+    }
+    // the table
+    var table = document.createElement("table");
+    table.classList.add("table");
+    table.classList.add("table-striped");
+    table.classList.add("table-bordered");
+    table.classList.add("table-hover")
+    // create headers
+    var tr = table.insertRow(-1); // TABLE ROW.
+    //var tableHeaders = ['Profile Name', 'Username', 'Text', 'Query Score'];
+    for (var i = 0; i < col.length; i++) {
+      var th = document.createElement("th"); // TABLE HEADER.
+      th.innerHTML = displayColumns[i];
+      th.style.textAlign = "center";
+      tr.appendChild(th);
+    }
+    // add rows
+    for (var i = 0; i < myData.length; i++) {
+      tr = table.insertRow(-1);
+      for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = myData[i][col[j]];
+      }
+    }
+    // Output
+    var resultWording = document.getElementById("resultWord");
+    var num = (noOfDocs >= 100) ? 100 : noOfDocs;
+    resultWording.innerHTML = div.innerHTML + "Displaying Top " + num + " Search Results";
+    resultWording.style.textAlign = "center";
+
+    var resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = div.innerHTML + noOfDocs + " Docs found in " + querySpeed + " milliseconds!";
+    resultsDiv.style.textAlign = "center";
+    //The (preparationTime + processingTime) can also be used here
+
+    var divContainer = document.getElementById("resultsDiv");
+    divContainer.innerHTML = "";
+
+    for (var i = 1; i < table.rows.length; i++) {
+      row = table.rows[i].getElementsByTagName("td");
+      //console.log(row);
+      var outerCard = document.createElement("div");
+      outerCard.className = 'card mx-3 my-3 px-3 py-3 w-50';
+      var title = document.createElement("h6");
+      title.innerHTML = row[0].innerHTML + " (<i>@" + row[1].innerHTML + "</i>)";
+      var body = document.createElement("div");
+      body.innerHTML = row[2].innerHTML;
+      body.style.marginBottom = "20px";
+      var score = document.createElement("div");
+      score.innerHTML = "<b>Query Score: </b>" + row[3].innerHTML;
+      outerCard.appendChild(title);
+      outerCard.appendChild(body);
+      outerCard.appendChild(score);
+      divContainer.appendChild(outerCard);
+    }//Creating a card for each result
+  }
---
+++ wordcloud.js
@@ -0,0 +1,55 @@
+am5.ready(async function() { 
+ 
+    // Create root element 
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element 
+    var root = am5.Root.new("chartdiv"); 
+    // Set themes 
+    // https://www.amcharts.com/docs/v5/concepts/themes/ 
+    root.setThemes([ 
+      am5themes_Animated.new(root) 
+    ]); 
+    // Add series 
+    // https://www.amcharts.com/docs/v5/charts/word-cloud/ 
+    var series = root.container.children.push(am5wc.WordCloud.new(root, { 
+      categoryField: "key", 
+      valueField: "value", 
+      maxFontSize: am5.percent(15) 
+    })); 
+    // Configure labels 
+    series.labels.template.setAll({ 
+      fontFamily: "Courier New" 
+    }); 
+    // code for animation 
+    setInterval(function() {   
+      am5.array.each(series.dataItems, function(dataItem) { 
+        var value = Math.random() * 65; 
+        value = value - Math.random() * value; 
+        dataItem.set("value", value); 
+        dataItem.set("valueWorking", value); 
+      }) 
+    }, 5000) 
+     
+    let url = "http://localhost:8983/solr/tweets/query?q=*:*&q.op=OR&indent=true&facet=true&facet.query=*:*&facet.field=Text"; 
+    var response = await fetch(url); 
+    var data = await response.json(); 
+    const values = {}; 
+    var count = []; 
+    var words = []; 
+    for (var i = 0; i < data.facet_counts.facet_fields.text.length; i += 2) { 
+        words.push(data.facet_counts.facet_fields.text[i]); 
+        data.facet_counts.facet_fields.text[i+1] && count.push(data.facet_counts.facet_fields.text[i + 1]); 
+    } 
+    var list = []; 
+    for(var i=0; i<words.length; i++){ 
+      var item = {}; 
+      item[words[i]] = count[i]; 
+      list.push(item); 
+    } 
+    // Data from: 
+    // https://insights.stackoverflow.com/survey/2021#section-most-popular-technologies-programming-scripting-and-markup-languages 
+    for(var i=0; i<words.length; i++){ 
+      series.data.push({ 
+        key : words[i], value : count[i] 
+      }); 
+    } 
+    }); // end am5.ready()
